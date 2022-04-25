431d531acc53e8985d9e4d5b744b1e26924df41e
ChetanDiploma <dhangarchetan40@gmail.com>
2022-04-25 13:52:53 +0530

    added:[chetan] c program basic 2 numbers for addition

---
+++ 2NUMBERS.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{       int a,b,c=0;
+	clrscr();
+        printf("\nEnter the 2 Number ");
+        scanf("%d,%d",&a,&b);
+        c=a+b;
+        printf("\nAddition=%d",c);
+        getch();
+}
