48d429f24e135f7fcba11e4272c68bac2383a4a5
ChetanDiploma <dhangarchetan40@gmail.com>
2022-04-25 13:20:24 +0530

    added:[chetan] c language basic program for measurement

---
+++ BASIC.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float inch=42,feet=0,meter=0,cm=0;
+        clrscr();
+        feet=inch/12;
+        printf("\nfeet=%f",feet);
+        meter=feet/3.5;
+        printf("\nmeter=%f",meter);
+        cm=inch*2.5;
+        printf("\ncm=%f",cm);
+        getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
